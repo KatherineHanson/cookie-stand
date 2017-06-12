3bcacda370b6b1ddaaf8d4b648fa49ad4ac48bfe
KatherineHanson <katherinemariehanson@gmail.com>
2017-06-12 13:03:07 -0700

    renamed HTML file, added pseudocode

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-alert('Test!');
+// First, create a separate JS object literal (no constructor functions... yet)
+// for each shop location that does the following:
+  // Stores the min/max hourly customers, and the average cookies per customer,
+  // in object properties
+  // Uses a method of that object to generate a random number of customers
+  // per hour. Objects/Math/random
+  // Calculate and store the simulated amounts of cookies purchased for
+  // each hour at each location using average cookies purchased and the
+  // random number of customers generated
+  // Store the results for each location in a separate array...
+  // perhaps as a property of the object representing that location
+  // Display the values of each array as unordered lists in the browser
+  // Calculating the sum of these hourly totals; your output for each location
+  // should look as shown in assignment
+  // Display the lists on sales.html. We will be adding features to this
+  // application and working with its layout throughout the week.
